5c9dd5ba7614531c8c105b69805ef6c4ca7a9ef4
janix374 <janko.kostic@yahoo.com>
2021-07-25 23:01:49 +0200

    firebase configuration

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -8,12 +8,6 @@ const firebaseConfig = {
 	storageBucket: 'air-ship-blog.appspot.com',
 	messagingSenderId: '1029379020245',
 	appId: '1:1029379020245:web:2340111a6fae5f0a7d595c',
-	// apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	// authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	// projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	// storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	// messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-	// appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
